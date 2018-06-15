83c5c3280d16d1afcef3d4e2264241cd451c8759
Harry Keartland <harrykeartland@gmail.com>
2018-06-16 00:01:27 +0100

    simplified nth term fractions

--- main.js
+++ main.js
@@ -8,11 +8,30 @@ function run()
 	c = Math.floor((Math.random() * scale) + 2);
 	r = Math.floor((Math.random() * scale) + 2);
 	eq = ""
+	g = gcd(gcd(a,b),c)
+	a /= g
+	b /= g
+	c /= g
 	for (j = 1; j <= terms; j++){
-       eq += String(a * (Math.pow(r, j)) + b) + "/" + String(c);
-       eq += ", ";
+		red = reduce((a * Math.pow(r, j)) + b,c)
+		fract = red[1] == 1 ? red[0] : red[0] + "/" + red[1]
+		eq += fract +", ";
 	}
-	ans = "<br>Nth term expression: (" + String(a) + " * " + String(r) + "ⁿ + " + String(b) + ")" + " / " + String(c) +"<br>"
+	ans = "<br>Nth term expression: ("
+	ans = a == 1 ? ans + r + "ⁿ + " + b + ")" : ans + a + " * " + r + "ⁿ + " + b + ")"
+	ans = c == 1 ? ans.replace(/(\(|\))/g,"") : ans + " / " + c
 	document.getElementById("container").innerHTML  += "<br><b>Q"+(i +1) + ".</b><div>"+ eq +"</div><button class='ans' id=\""+i+"b\" onclick=\"document.getElementById('"+i+"b').outerHTML ='<div>"+ans+"</div>'\">Ans</button>"
 	i++
 	}
+
+function gcd(a,b){
+	return b ? gcd(b, a%b) : a;
+}
+
+
+function reduce(numerator,denominator){
+	numerator = Math.round(numerator)
+	denominator = Math.round(denominator)
+	g = gcd(numerator,denominator);
+  return [numerator/g, denominator/g];
+}
